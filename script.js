237e68605731f5e067edb1a4678c14cb01107b45
Selina Su <phongsu20@gmail.com>
2022-09-04 09:38:36 -0700

    resubmit

--- script.js
+++ script.js
@@ -1,102 +1,46 @@
 // Assignment Code Here
-var criterias = {
-  length: "",
-  lowerCase: "",
-  upperCase: "",
-  number: "",
-  symbol: "",
-}
-
 var dataList = {
-  dataLowerCase: "abcdefghijklmnopqrstuvwxyz",
-  dataUpperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  dataNumber: "0123456789",
-  dataSymbol: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+  dataLowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  dataUpperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  dataNumber: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  dataSymbol: ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"],
 }
-var enter;
+var enter = [];
 var generateBtn = document.querySelector("#generate");
-var allCharacters = "";
-var result = "";
-var myLength = 0;
+var allCharacters = [];
 
-// Function to choose Criterias
+// Function to generate Password
 
-var chooseCriteria = function() {
-  criterias.lowerCase = prompt("Do you want lower cases in your password? Answer Yes or No.");
-  if  (criterias.lowerCase === "yes") {
-    alert("You will have lower cases in  your password");
-    criterias.lowerCase = true;
-    allCharacters = dataList.dataLowerCase;
-  } else if (criterias.lowerCase === "no") {
-    alert("No lower case choosen.");
-    criterias.lowerCase = false;
-  }
+function generatePassword() {
 
-  criterias.upperCase = prompt("Do you want upper cases in your password? Answer Yes or No.");
-  if (criterias.upperCase === "yes") {
-    alert("You will have upper cases in your password");
-    criterias.upperCase = true;
-    allCharacters = allCharacters + dataList.upperCase;
-  } else if (criterias.upperCase === "no") {
-    alert("No upper cases choosen.");
-    criterias.upperCase = false;
-  }
+var result = prompt ("Please choose from 8 to 128 characters.");
+var lowercase = confirm("Do you want lower cases in your password?");
+var uppercase = confirm("Do you want upper cases in your password?");
+var number = confirm("Do you want numbers in your password?");
+var symbol = confirm("Do you want symbols in your password?");
 
-  criterias.number = prompt("Do you want numbers in your password? Answer Yes or No.");
-  if (criterias.number === "yes") {
-    alert("You will have numbers in your password");
-    criterias.number = true;
-    allCharacters = allCharacters + dataList.number;
-  } else if (criterias.number === "no") {
-    alert("No numbers choosen.");
-    criterias.number = false;
-  }
-
-  criterias.symbol = prompt("Do you want symbols in your password? Answer Yes or No.");
-  if (criterias.symbol === "yes") {
-    alert("You will have symbols in your password");
-    criterias.symbol = true;
-    allCharacters = allCharacters + dataList.symbol;
-  } else if (criterias.symbol === "no") {
-    alert("No symbol choosen.");
-    criterias.symbol = false;
-  }
-
-  if (!criterias.lowerCase && !criterias.upperCase && !criterias.number && !criterias.symbol) {
-    alert("Please choose yes to at least one criteria");
-  }
-console.log ("first function" + chooseCriteria);
+if (lowercase) {
+  allCharacters = allCharacters.concat(dataList.dataLowerCase);
 }
 
-function findResult() {
-  for (var i = 0, n = allCharacters.length; i < myLength; i++) {
-  result += allCharacters.charAt(Math.floor(Math.random() * n)); 
-}
-console.log ("find result" + result); 
+if (uppercase) {
+  allCharacters = allCharacters.concat(dataList.dataUpperCase);
 }
 
+if (number) {
+  allCharacters = allCharacters.concat(dataList.dataNumber);
+}
 
-// function to enter password length
+if (symbol) {
+  allCharacters = allCharacters.concat(dataList.dataSymbol);
+}
+console.log ("check Characters " + allCharacters);
 
-var generatePassword = function() {
-  enter = parseInt(prompt("Please choose from 8 to 128 characters."));
-  if (!enter) {
-    alert("Please enter a number.");
-  } else if (enter < 8 || enter > 128) {
-    alert("Please choose a number from 8 to 128");
-  } else {
-    alert("Valid choice! Thank you!");
-    myLength = enter;
-    console.log("enter value: " + enter);
-  }
-    alert("Please choose password criterias.");
-    chooseCriteria();
-    console.log ("test " + chooseCriteria);
-    alert("Generating your password...");
-    findResult();
-    console.log ("what is my result " + findResult());
-    alert ("Your password is " + result);
-    return result;
+for (var i = 0, n = allCharacters.length; i < result; i++) {
+    enter.push (allCharacters[Math.floor(Math.random() * n)]);                     
+}
+
+return enter.join("");
 }
 
 // Write password to the #password input
